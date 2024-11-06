9642653facaa616aacfa1ef41f62e73843cb7aea
M-Hasssan <muhammed.hassan.tariq@gmail.com>
2024-11-06 18:27:15 +0500

    Task Item UI

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Todays's Task
+        </Text>
+
+        <View style={styles.item}>
+          <Task text={'task 1'} />
+          <Task text={'task 2'} />
+
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +23,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+
+  wrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  item: {
+    marginTop: 30,
   },
 });
