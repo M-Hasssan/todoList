c99602e6a897ee764d514a9822a0ebc72c8fb961
M-Hasssan <muhammed.hassan.tariq@gmail.com>
2024-11-08 22:14:26 +0500

    Push

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+
+const [task, setTask] = useState();
+
+const [taskItems, setTaskItems] = useState([]);
+
+
+const handleAddTask = () => {
+  Keyboard.dismiss();
+  setTaskItems([...taskItems, task])
+  setTask(null);
+};
+
+const completeTask = (index) => {
+  let itemsCopy = [...taskItems];
+  itemsCopy.splice(index, 1);
+  setTaskItems(itemsCopy);
+}
+ 
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
@@ -10,17 +29,45 @@ export default function App() {
         </Text>
 
         <View style={styles.item}>
-          <Task text={'task 1'} />
-          <Task text={'task 2'} />
+
+          {
+            taskItems.map((item, index) => {
+             return (
+             <TouchableOpacity key={index} onPress={() => completeTask()}>
+              <Task  text={item} />
+             </TouchableOpacity>
+             )
+            })
+          }
 
         </View>
 
       </View>
+      
+
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper }
+      >
+
+        
+
+        <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text =>   setTask(text)} />
+
+        <TouchableOpacity
+        onPress={() => handleAddTask()}
+        style={styles.addWrapper}>
+          <View style={styles.addWrapper}>
+           <Text style={styles.taskAdd}>+</Text>
+           </View>
+            </TouchableOpacity>   
+
+      </KeyboardAvoidingView>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
   container: {
     flex: 1,
     backgroundColor: '#E8EAED',
@@ -37,4 +84,36 @@ const styles = StyleSheet.create({
   item: {
     marginTop: 30,
   },
+  writeTaskWrapper : {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+
+  },
+
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor:  '#C0C0C0',
+    borderWidth: 1,
+  },
+
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor:  '#C0C0C0',
+    borderWidth: 1,
+  },
+
+  taskAdd: {},
 });
